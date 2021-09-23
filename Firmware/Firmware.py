0c2400984ace6a1b0b36153d2d1558cd6b9fbc8b
nauman3s <naumanshakir3s@gmail.com>
2021-09-23 08:54:07 +0500

    updated ckt, fw

--- Firmware/Firmware.py
+++ Firmware/Firmware.py
@@ -1,25 +1,46 @@
-from mq import *
-import sys, time
+import sys
+import time
+from gas_detection import GasDetection
 
-BAC= 10000#1BAC(g/dL) is 10000ppm
-PPM=1/BAC
+BAC = 10000  # 1BAC(g/dL) is 10000ppm
+PPM = 1/BAC
 
-mq = MQ()
+detection = GasDetection()
+print('Please wait for 10 seconds while the Sensor Calibrates itself.')
+time.sleep(10)
+loopCount = 0
+valsArray = []
+
+
 def getAlcoholValue():
-    global mq,BAC,PPM
-    try:
-    
-        perc = mq.MQPercentage()
-        print("ALCOHOL: %g ppm" % (perc["ALCOHOL"]))
-        BACValue=(perc["ALCOHOL"])*PPM
-        print('ALCOHOL BAC=',BACValue)
-        return BACValue
+    global PPM, BAC, detection
 
-    except:
-        print("\nAbort by user")
+    print('Calibrating ...')
+
+    try:
+
+        ppm = detection.percentage()
+
+        ppmValue = ppm[detection.ALCOHOL_GAS]
+        BACValue = ppmValue*PPM
+        values = [ppmValue, BACValue]
+
+        return values
+
+    except Exception as e:
+        print('\nAborted!')
+
 
 while True:
 
-    getAlcoholValue()
+    v = getAlcoholValue()
+    print('ALCOHOL: {} ppm'.format(v[0]))
+    print('ALCOHOL: {} BAC'.format(v[1]))
+    valsArray.append(v[1])
+    loopCount = loopCount+1
+    if(loopCount > 10):
+        avg = sum(valsArray)/10.0
+        print('ALCOHOL 10 Seconds Average: {} BAC'.format(avg))
+        loopCount = 0
 
     time.sleep(1)
